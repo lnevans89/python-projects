967a73251a300db4be320385046aa01c0ecc0206
lnevans89 <lnevans89@gmail.com>
2025-01-11 17:49:38 -0600

    Update BandNameGenerator.py

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,4 +1,4 @@
 print("Welcoome to Band Name Gnerator")
 q1=input("What's the name of the of the city you grew up in?\n ")
 q2=input("What's your pets name?\n")
-print("")
+print("Your band name could be" +q1+ " " +q2 )
