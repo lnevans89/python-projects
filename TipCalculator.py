3c6bacb92e162267ef43aa21afe982eb7faf4984
lnevans89 <lnevans89@gmail.com>
2025-01-12 19:49:47 -0600

    Update TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,3 +1,4 @@
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
-input("What percentage tip would you like to give?")
+tip = int(input("What percentage tip would you like to give? 10 12 15"))
+people = int(input("How many people to split the bill?"))
